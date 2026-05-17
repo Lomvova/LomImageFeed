7479ff4af64714841a1e946039c39d46eb819cf6
Lomvova <ilomvova@icloud.com>
2026-05-17 14:23:47 +0400

    Добавил таблицу

---
+++ Image Feed/ImagesListViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
