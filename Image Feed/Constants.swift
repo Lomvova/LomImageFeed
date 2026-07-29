8e9e61199131024adf6c28c3a850f584c3f8ac82
Lomvova <ilomvova@icloud.com>
2026-07-29 21:28:08 +0400

    Ревью спринт 10.1

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "8q1QV0b1o_4pdzEQeDOv5vKD2rP-6wbgUgzlfFNCvLs"
+    static let secretKey = "CBKYCOEFsOMXp7GZrwveDqaKSc3mUUaLpKyPuA7kmCU"
+    static let redirectURI = "dPFktcqxi0PmR04uDGK0tv1t1p9vPYOzo-v00MV24T4"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURLString = URL(string: "https://api.unsplash.com")!
+}
